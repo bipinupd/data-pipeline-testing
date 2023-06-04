97190fc59fbc1375ba1b476caa2b499c2de2dd22
Bipin Upadhyaya <bipinu@google.com>
2023-06-04 10:40:20 -0400

    Store info app

--- store-info-pipeline/setup.py
+++ store-info-pipeline/setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="pubsub-to-bq-streaming-store",
+    name="store-info-app",
     version=0.1,
     packages=setuptools.find_packages(),
     install_requires=[ 'apache-beam[gcp]'])
