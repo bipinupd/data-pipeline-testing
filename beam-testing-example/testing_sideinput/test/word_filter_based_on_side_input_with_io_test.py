56ac581280d8837dab8ab3c601f6e19251534c8d
Bipin Upadhyaya <bipinu@google.com>
2023-06-06 12:08:41 -0400

    CRC32 check for GCS objects

--- beam-testing-example/testing_sideinput/test/word_filter_based_on_side_input_with_io_test.py
+++ beam-testing-example/testing_sideinput/test/word_filter_based_on_side_input_with_io_test.py
@@ -11,10 +11,23 @@ class WordExtractingDoFn_Pipeline_WithIO_Test(unittest.TestCase):
         self.project = self.test_pipeline.get_option('project')
         self.test_bucket = self.test_pipeline.get_option('test_bucket')
 
+    def _getcrc32_for_objects(self,bucket_name, prefix_name):
+        from google.cloud import storage
+        client = storage.Client()
+        if (bucket_name.startswith('gs://')):
+            bucket_name=bucket_name.replace('gs://', '')
+        blob_list = client.list_blobs(bucket_name, prefix=prefix_name)
+        compute_output_crc = []
+        for blob in blob_list:
+            if blob.name != prefix_name:
+                compute_output_crc.append(blob.crc32c)
+        return compute_output_crc
+    
     def tearDown(self):
         os.system(
             f"gsutil rm -rf {self.test_bucket}/{self.pipeline_name}/{self._testMethodName}/output/"
         )
+    
     def test_end_to_end_happy_path(self):
         extra_opts = {
             'project': self.project,
@@ -26,6 +39,10 @@ class WordExtractingDoFn_Pipeline_WithIO_Test(unittest.TestCase):
         word_filter_based_on_side_input.run(
             self.test_pipeline.get_full_options_as_args(**extra_opts),
             save_main_session=True)
+        
+        crc_output = self._getcrc32_for_objects(self.test_bucket,f"{self.pipeline_name}/{self._testMethodName}/output/")
+        crc_expected_output = self._getcrc32_for_objects(self.test_bucket,f"{self.pipeline_name}/{self._testMethodName}/expected_output/")
+        assert(crc_output.sort() == crc_expected_output.sort())
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
